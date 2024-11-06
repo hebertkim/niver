067d024087e93e297583e11c23d56890b461403e
hebertkim <hebert.kim@hotmail.com>
2024-11-06 16:47:18 -0300

    Projeto Limpo

--- src/components/AuthorSection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <section id="author" class="py-12 text-center">
-      <img src="link_to_author_image.jpg" alt="Autor" class="w-32 h-32 rounded-full mx-auto" />
-      <p class="mt-4">Com mais de 10 anos ajudando pessoas a transformarem suas vidas através da alimentação e exercícios.</p>
-    </section>
-  </template>
-  
--- src/components/BonusSection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <section id="bonus" class="py-12 bg-gray-100 text-center">
-      <h2 class="text-3xl font-bold">Bônus Exclusivo para Quem Baixar Agora</h2>
-      <p class="mt-4">Além do eBook, você recebe uma lista exclusiva de receitas fitness e acesso a um grupo de apoio no WhatsApp.</p>
-    </section>
-  </template>
-  
